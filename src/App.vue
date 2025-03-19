1abc46956e77fa8ec5a48e8ec5ae4ca4a9f11bb9
radclim <mmajaaaa@gmail.com>
2025-03-19 19:42:15 +0100

    fix: saves the correct albums

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,17 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import MainHeader from './components/MainHeader.vue'
-const albums = ref(null)
+const albums = ref([])
 const count = ref(0)
 
 const fetchUserData = async () => {
   try {
     const response = await fetch('https://www.theaudiodb.com/api/v1/json/2/album.php?i=111752')
     const data = await response.json()
-    albums.value = data.album
-    for (let album of albums.value) {
+    for (let album of data.album) {
       if (album.strReleaseFormat == 'Album') {
         count.value++
+        albums.value.push(album)
       }
     }
   } catch (error) {
